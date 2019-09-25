b590c440ae618a0984abd9a1674198d612d3b71d
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-24 21:51:44 -0400

    finished the project

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,26 @@
 import Foundation
 
 func expandTheNumber(_ number: Int) -> [Int] {
+
+    guard number > 0 else { return [0] }
+    var answer: [Int] = []
+    var i = 1
     
+    while true {
+        let x = number % Int(pow(Double(10), Double(i)))
+        let sum = answer.reduce(0) { (result, next) -> Int in
+            return result + next
+        }
+        
+        let result = x - sum
+        if sum >= number {
+            break
+        }
+        
+        answer.append(result)
+        i += 1
+    }
+    return answer.reversed()
 }
 
 expandTheNumber(199)  // [100, 90, 9]
@@ -10,3 +29,4 @@ expandTheNumber(0)    // [0]
 expandTheNumber(562)  // [500, 60, 2]
 expandTheNumber(5280) // [5000, 200, 80, 0]
 expandTheNumber(560)  // [500, 60, 0]
+
