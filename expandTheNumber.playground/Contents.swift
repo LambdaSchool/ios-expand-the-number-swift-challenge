09894d5e63e56e8f33f99e636b3c560c871a822e
Craig Swanson <cswanson02@me.com>
2019-10-01 20:31:11 -0500

    couldn't get the number to change back to Int

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,22 @@
 import Foundation
 
 func expandTheNumber(_ number: Int) -> [Int] {
+    let numberString = String(number)
+    let digits = numberString.count
     
+    var array: [Int] = []
+    for n in 0...digits {
+        let digit = numberString[numberString.index(numberString.startIndex, offsetBy: (numberString.count - n))]
+        let number = Int(digit)
+        array.append(number)
+    }
+    }
+    
+
+//    switch digits {
+//    case 0...5:
+
+
 }
 
 expandTheNumber(199)  // [100, 90, 9]
@@ -10,3 +25,11 @@ expandTheNumber(0)    // [0]
 expandTheNumber(562)  // [500, 60, 2]
 expandTheNumber(5280) // [5000, 200, 80, 0]
 expandTheNumber(560)  // [500, 60, 0]
+
+let numb = 1000
+let numbString = String(numb)
+numbString.count
+numbString[numbString.startIndex]
+numbString[numbString.index(numbString.startIndex, offsetBy: (numbString.count - 1))]
+
+
