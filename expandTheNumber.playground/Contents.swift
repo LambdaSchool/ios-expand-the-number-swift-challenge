7fc5bd92dd760e1f442b7a8b0805ec17e626a53e
Joseph <JoeRogers@lambdasr200smbp.attlocal.net>
2019-10-01 20:12:09 -0500

    Completed

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,21 @@
 import Foundation
 
 func expandTheNumber(_ number: Int) -> [Int] {
-    
+    var number = number
+    var TenIndex = 10
+    var intArray: [Int] = []
+
+    if number == 0 {
+        intArray.append(0)
+    } else {
+        repeat {
+            let appendingNumber = number % TenIndex
+            intArray.append(appendingNumber)
+            number = number - appendingNumber
+            TenIndex = TenIndex * 10
+        } while number > 10
+    }
+    return intArray.reversed()
 }
 
 expandTheNumber(199)  // [100, 90, 9]
@@ -10,3 +24,5 @@ expandTheNumber(0)    // [0]
 expandTheNumber(562)  // [500, 60, 2]
 expandTheNumber(5280) // [5000, 200, 80, 0]
 expandTheNumber(560)  // [500, 60, 0]
+
+
