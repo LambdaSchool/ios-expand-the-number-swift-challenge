7c7d3642746b0ddab2f0e09f56034e5453e9a4dc
Patrick Millet <lohs79seahawk@gmail.com>
2019-11-13 10:32:50 -0600

    finished coding challenge

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -2,7 +2,28 @@ import Foundation
 
 func expandTheNumber(_ number: Int) -> [Int] {
     
+    var passedNumber = number
+    var tensPlaceholder = 10
+    var numberArray: [Int] = []
+    
+    if passedNumber == passedNumber {
+        switch passedNumber {
+        case 1...10:
+            numberArray.append(passedNumber)
+        default:
+            repeat {
+            let bigNumber = passedNumber % tensPlaceholder
+            numberArray.append(bigNumber)
+            passedNumber = passedNumber - bigNumber
+            tensPlaceholder = tensPlaceholder * 10
+            } while passedNumber > 10
+        }
+    }
+    return numberArray.reversed()
 }
+
+
+
 
 expandTheNumber(199)  // [100, 90, 9]
 expandTheNumber(100)  // [100, 0, 0]
@@ -10,3 +31,4 @@ expandTheNumber(0)    // [0]
 expandTheNumber(562)  // [500, 60, 2]
 expandTheNumber(5280) // [5000, 200, 80, 0]
 expandTheNumber(560)  // [500, 60, 0]
+expandTheNumber(6)
