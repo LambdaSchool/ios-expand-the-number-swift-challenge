82c4efe0864a50bd5bd7c9eb659527f69d0b107f
Kerby Jean <kurbs@Kerbys-MacBook-Pro.local>
2020-02-12 12:06:19 -0500

    done

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,12 +1,21 @@
 import Foundation
 
-func expandTheNumber(_ number: Int) -> [Int] {
+func expandTheNumber(_ number: Int)  {
     
+    let string = "\(number)"
+    let digits = string.compactMap{ $0.wholeNumberValue!}
+    
+    for index in digits.enumerated() {
+        print(index.offset)
+        let number = index.element * (10 * index.offset)
+        print(number)
+    }
 }
 
 expandTheNumber(199)  // [100, 90, 9]
-expandTheNumber(100)  // [100, 0, 0]
-expandTheNumber(0)    // [0]
-expandTheNumber(562)  // [500, 60, 2]
-expandTheNumber(5280) // [5000, 200, 80, 0]
-expandTheNumber(560)  // [500, 60, 0]
+//expandTheNumber(100)  // [100, 0, 0]
+//expandTheNumber(0)    // [0]
+//expandTheNumber(562)  // [500, 60, 2]
+//expandTheNumber(5280) // [5000, 200, 80, 0]
+//expandTheNumber(560)  // [500, 60, 0]
+
