b56218e1e21e7f99505c2e69da48e9fcdeba7405
Chris <chris.gonzales.82@gmail.com>
2020-02-12 08:45:34 -0800

    Challenge attempt

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,6 +1,33 @@
 import Foundation
 
+//extension Int: IteratorProtocol{
+//    public typealias Element = Int
+//
+//}
+
 func expandTheNumber(_ number: Int) -> [Int] {
+    
+    
+    // need to iterate over Int as an array.  Convert int to a string array? If index 0, return number in place, for all other indices multiply index by 10, update array
+    var newIntArray: [Int] = []
+    
+ let numberString = String(number)
+    print(numberString)
+    
+    for character in numberString{
+        let newNumber = character.hashValue
+        newIntArray.append(newNumber)
+    }
+    
+//    if newIntArray[0]{
+//
+//    }
+    
+    
+    
+    
+    
+    return newIntArray
     
 }
 
