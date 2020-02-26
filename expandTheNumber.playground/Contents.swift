0eb714106a8bed6e4d58507d606cf22560cc1d57
Mark Gerrior <github@gerrior.com>
2020-02-26 08:25:12 -0800

    Completed using a while loop

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,23 @@
 import Foundation
 
-func expandTheNumber(_ number: Int) -> [Int] {
+func expandTheNumber(_ number2: Int) -> [Int] {
+    var number = number2 // number2 is a 'let' constant. Store locally so we can change it
+    var result: [Int] = []
+    var iteration = 1
     
+    while number > 0 {
+        let remainder = number % 10
+        
+        //print(remainder, iteration)
+        result.insert(remainder * iteration, at: 0)
+
+        // Prepare for next iteration
+        number = number / 10
+        iteration *= 10
+    }
+    
+    if result.count == 0 { result.insert(0, at: 0) }
+    return result
 }
 
 expandTheNumber(199)  // [100, 90, 9]
