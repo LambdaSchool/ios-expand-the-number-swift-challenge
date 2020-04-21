cb2c17774e33c06924d4ff616fa6a3d99dc68352
Libby Thomas <thomaslibby05@gmail.com>
2020-04-20 21:31:33 -0500

    Code Challenge Complete

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -2,6 +2,32 @@ import Foundation
 
 func expandTheNumber(_ number: Int) -> [Int] {
     
+    var numberArray: [Int] = []
+    
+    let ones = number % 10
+    let tens = (number - ones) % 100
+    let hundreds = (number - ones - tens) % 1000
+    let thousands = (number - ones - tens - hundreds) % 10000
+    
+    
+    if number > 999 {
+        numberArray.append(thousands)
+    }
+    if number > 99 {
+        numberArray.append(hundreds)
+    }
+    if number > 9 {
+        numberArray.append(tens)
+    }
+    if number > 0 {
+        numberArray.append(ones)
+    }
+    if number == 0 {
+        numberArray.append(0)
+    }
+
+    
+    return numberArray
 }
 
 expandTheNumber(199)  // [100, 90, 9]
